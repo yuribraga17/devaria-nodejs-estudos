f9946cb85dbc3b4a1d0e382d2c6b1acb4103d68c
Yuri Braga <yuribragasoares@gmail.com>
2022-12-19 00:09:50 -0300

    Atualização de código
    Organizado pasta de aulas;
    Adicionado kart lista com nome de Douglas e Rafael na lista, checagem de habilitação e idade +18 também.

---
+++ aula02/kart-lista.js
@@ -0,0 +1,35 @@
+const { read } = require('fs');
+const { Readline } = require('readline/promises');
+
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Este programa vai checar se você é maior de 18 anos e tem habilitação para saber se você pode entrar no kart.');
+console.log('Além de suas verificações, precisamos checar se você está na lista de agendamento do horário');
+
+readLine.question('Qual seu ano de nascimento?', ano => {
+    if(ano > 2004){
+        console.log('Você não tem 18 anos!');
+    }else{
+        readLine.question("Você tem habilitação? (Sim/Não)", temHabilitacao =>{
+            if(!(temHabilitacao.toUpperCase() === "SIM")){
+                console.log('Você não tem habilitação para adentrar a pista de kart');
+            }else{
+                readLine.question("Qual o seu nome?", nome =>{
+                    switch(nome){
+                        case 'Douglas' :
+                            console.log('Bem vindo ao kart, Douglas!');
+                            break;
+                        case 'Rafael' :
+                            console.log('Bem vindo ao kart, Rafael!');
+                            break;
+                        default:
+                            console.log('Você não está na lista de presença');
+                    }
+                });
+            }
+        })
+    }
+})
